de8ce8b7d73f20071af6ad57fa57d11ac0d60805
plaetzaw <plaetzaw@gmail.com>
2021-06-07 19:00:13 -0500

    Adjusting php settings

--- mailer/form_setting.php
+++ mailer/form_setting.php
@@ -1,7 +1,7 @@
 <?php
 	/*Form settings*/
 	$subj = "New message from the site 'Myour - Personal Portfolio Template'"; //letter subject
-	$to = 'test@test.com'; // Enter Your E-mail
+	$to = 'plaetzaw@gmail.com'; // Enter Your E-mail
 	$from = 'admin@you-site-name.com'; // Admin e-mail
 	$fromName = 'Your Company Name'; // Your company name
 	$charset = 'UTF-8';
